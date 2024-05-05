0ea82a3b3ed7a5f9cacc2cec71f3518093121db6
kaddie1 <nel16064@byui.edu>
2024-05-04 22:09:19 -0600

    mission_ready

--- mission/mission.js
+++ mission/mission.js
@@ -1,5 +1,5 @@
 
-/* I got help from the code in class */
+/* I used the code from in class and chatgpt also helped me */
 
 document.addEventListener('DOMContentLoaded', function () {
     const selectElement = document.getElementById('theme');
@@ -7,8 +7,13 @@ document.addEventListener('DOMContentLoaded', function () {
     selectElement.addEventListener('change', function () {
 
         const selectedValue = this.value;
+
         if (selectedValue === 'dark') {
             document.body.classList.add('dark');
+
+            var image = document.getElementById("image");
+            image.src = "byuilight.png";
+
         } /* end if*/
 
         else{
