c83bd634dc225f909cdc3fa2d909fb697c5320ba
kaddie1 <nel16064@byui.edu>
2024-05-04 21:24:52 -0600

    mission statement 2

--- mission/mission.js
+++ mission/mission.js
@@ -1,1 +1,21 @@
 
+/* I got help from the code in class */
+
+document.addEventListener('DOMContentLoaded', function() {
+    const selectElement = document.getElementById('theme');
+
+    selectElement.addEventListener('change', function () {
+
+        const selectedValue = this.value;
+        if (selectedValue === 'dark') {
+            document.body.classList.add('dark');
+        } /* end if*/
+
+        else
+        {
+            document.body.classList.remove('dark');
+            
+        }
+
+    });
+});
