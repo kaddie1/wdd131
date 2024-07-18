b83278a8520788d533cdc2700458817adf6da3a3
kaddie1 <nel16064@byui.edu>
2024-07-18 15:55:23 -0600

    jamdown_restaurant_final_project

---
+++ Jamdown_restaurant/main.js
@@ -0,0 +1,120 @@
+import food_items from './restaurant.mjs';
+
+food_items.forEach((food_items)=>{
+    // Create a food_item element and store it in a variable
+    let food_items_Article = document.createElement('article')
+	
+
+
+
+	let food_items_info = `
+	
+    <p class = "display_name">${food_items.name}</p>
+    
+    <div class = "image_format">
+    <img src="${food_items.image}">
+    </div>
+    <p class = "display_about">${food_items.About}</p>
+    <p class = "display_price">Price: $ ${food_items.price}</p>
+    
+    `
+
+    food_items_Article.innerHTML = food_items_info
+    
+    let food_items_Holder = document.getElementById("menu_layout")
+    food_items_Holder.appendChild( food_items_Article)
+       
+        
+
+})
+
+
+
+
+
+
+//Collect form information
+
+    // JavaScript to handle form submission
+    let submitform = document.getElementById('orderform_layout')
+    submitform.addEventListener('submit', function(event) {
+        // Prevent the default form submission
+        event.preventDefault();
+
+        // Create a FormData object from the form element
+        const formData = new FormData(event.target);
+
+        
+
+        // Get the selected value from the FormData object
+        const selectedName = formData.get('name');
+        const selectedCarbohydrate = formData.get('Carbohydrate');
+        const selectedProtein = formData.get('Protein');
+        const selectedBeverage = formData.get('Beverage');
+        const selectedBeverage_q = formData.get('Beverage_quantity');
+        const selectedProtein_q = formData.get('Protein_quantity');
+        const selectedCarbohydrate_q = formData.get('Carbohydrate_quantity');
+
+        let subtotal
+
+        
+        food_items.forEach((food_items)=>{
+
+
+            if (food_items.name == selectedCarbohydrate)
+
+            {
+
+                subtotal =subtotal + (food_items.price * selectedCarbohydrate_q)
+            } //end of if
+
+            else if (food_items.name == selectedProtein) {
+                subtotal =subtotal + (food_items.price * selectedProtein_q)
+
+            }
+
+            else if (food_items.name == selectedBeverage) {
+
+                subtotal =subtotal + (food_items.price * selectedBeverage_q)
+            }
+
+        })
+
+        let total 
+        let tax
+
+        tax= subtotal * 0.06
+        total = subtotal + tax
+        
+        submitform.style.display = "none";
+
+
+        let info = {
+            tax: tax,
+            total: total,
+            subtotal: subtotal
+
+           }
+
+        let submit_success = successTemplate(info)
+
+        const summary_info = document.getElementById("summary")
+        console.log(summary_info)
+      
+        summary_info.innerHTML = submit_success
+    });
+
+
+    function successTemplate(info) {
+        return `<div class="success-message">
+                  <p>Your Order was Successful!</p>
+                  <p>Subtotal: $${info.subtotal}</p>
+                  <p>Tax: $${info.tax}</p>
+                  <p>Total Fee: $${info.total}</p>
+                  <p>Thank you for choosing Jamdown Restaurant!</p>
+                </div>`;   
+              }
+    
+
+
+
---
+++ Jamdown_restaurant/restaurant.mjs
@@ -0,0 +1,90 @@
+
+const food_items = [
+	{
+
+name: 'Rice and Peas',
+image: "/Jamdown_restaurant/images/rice_peas.jpg",
+About: [
+    'Rice and kidney beans cooked with seasons and coconut milk',
+],
+price: 8
+// rating: 4
+},
+
+{
+name: 'White Rice',
+image: "/Jamdown_restaurant/images/whiterice.jpg",
+About: [
+    'White rice cooked to perfection',
+],
+price: 8
+// rating: 4
+},
+
+{
+name: 'Dumpling',
+image: "/Jamdown_restaurant/images/dumplings.jpg",
+About: [
+    'Cornmeal Cooked Dumplings',
+],
+price: 5
+// rating: 4
+},
+
+
+
+{
+    name: 'Jerk Chicken',
+    image: "/Jamdown_restaurant/images/jerkchicken.jpg",
+    About: ['Chicken seasoned with Jamaican spices and grilled in original Jamaican Jerk pan with pimento wood '],
+   
+    price: 10
+},
+
+
+{
+    name: 'Curried Chicken',
+    image: "/Jamdown_restaurant/images/curry_chicken.jpg",
+    About: ['Chicken cooked with curry season.'],
+   
+    price: 8
+},
+
+
+{
+    name: 'Ackee and Salt Fish',
+    image: "/Jamdown_restaurant/images/ackee_and_saltfish.jpg",
+    About: ['Seasoned ackee with saltfish. Goes well with dumplings'],
+   
+    price: 15
+},
+
+{
+    name: 'Ting',
+    image: "/Jamdown_restaurant/images/ting.jpg",
+    About: ['Grapefruit flavored carbonated soda'],
+   
+    price: 6
+},
+
+{
+    name: 'Ginger Beer',
+    image: "/Jamdown_restaurant/images/ginger.jpg",
+    About: ['Ginger flavored carbonated soda'],
+   
+    price: 5
+},
+
+{
+    name: 'Kola Champagne',
+    image: "/Jamdown_restaurant/images/champagne.jpg",
+    About: ['Kola Champagne flavored carbonated soda'],
+   
+    price: 5
+}
+
+]
+
+
+
+export default food_items
